6ef6db868f7b3d22a9ff75b9b5160b15493e2745
Adam Kattan <kattanadam7@gmail.com>
2025-03-09 20:14:41 +0100

    feat: added working method lookup

--- frontend/src/api/github/connect.ts
+++ frontend/src/api/github/connect.ts
@@ -1,34 +1,16 @@
 import { axiosInstance } from "../config";
 import { Project } from "../projects/types";
 
-const lookup_function = async (element: string, branch: string, project: Project) => {
-    const gh_url = `https://api.github.com/search/code?q=${element}+repo:${project.owner}/${project.repository}`;
-    const headers = {
-        "Accept": "application/vnd.github.v3+json",
-        "Authorization": project.accessToken ? `token ${project.accessToken}` : null
-    }
-    const resp = await axiosInstance.get(gh_url, { headers: headers });
-    if (!resp.data.items || resp.data.items.length === 0) {
-        console.log(`Function '${element}' not found in ${project.owner}/${project.repository}.`);
-        return;
-    }
+export const get_element_gh_url = async (name: string, ms: string, classPath: string, branch: string, project: Project) => {
 
-    const fileInfo = resp.data.items[0];
-    const filePath = fileInfo.path;
+    const dataResp = await axiosInstance.post("/github", {
+        "methodName": name,
+        "ms": ms,
+        "classPath": classPath,
+        "branch": branch,
+        "owner": project.owner,
+        "repository": project.repository
+    });
 
-    const fileUrl = `https://github.com/${project.owner}/${project.repository}/blob/${branch}/${filePath}`;
-
-    const fileDataUrl = `https://raw.githubusercontent.com/${project.owner}/${project.repository}/${branch}/${filePath}`;
-
-    const dataResp = await axiosInstance.get(fileDataUrl, { headers: headers });
-    const fileContent = dataResp.data;
-    const lines = fileContent.split("\n");
-
-    const lineNumber = lines.findIndex((line: string) => new RegExp(`\\b${element}\\b`).test(line)) + 1;
-    if (lineNumber > 0) {
-        const finalUrl = `${fileUrl}#L${lineNumber}`;
-    } else {
-        console.log(`Function '${element}' found in file '${filePath}', but line number could not be determined.`);
-    }
-
+    return dataResp.data;
 }
